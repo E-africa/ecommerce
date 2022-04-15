e35cea85ad0a853d75a994331a13efad405cbcaa
E-africa <efricashopping@gmail.com>
2022-04-16 00:11:23 +0300

    composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit09776eebc1335f974bf9af32d89d6fef
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit09776eebc1335f974bf9af32d89d6fef', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit09776eebc1335f974bf9af32d89d6fef', 'loadClassLoader'));
--- vendor/twilio/sdk/src/Twilio/VersionInfo.php
+++ vendor/twilio/sdk/src/Twilio/VersionInfo.php
@@ -6,8 +6,8 @@ namespace Twilio;
 
 class VersionInfo {
     const MAJOR = 6;
-    const MINOR = 31;
-    const PATCH = 2;
+    const MINOR = 36;
+    const PATCH = 1;
 
     public static function string() {
         return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
