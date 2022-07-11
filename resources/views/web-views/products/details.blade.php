822238761dee661908e6dd166bab026ff994bf92
E-africa <efricashopping@gmail.com>
2022-07-11 23:31:58 +0300

    fixed currency display

--- resources/views/web-views/products/details.blade.php
+++ resources/views/web-views/products/details.blade.php
@@ -261,6 +261,7 @@
                         <span
                             class="h3 font-weight-normal text-accent {{Session::get('direction') === "rtl" ? 'ml-1' : 'mr-1'}}" style="color: #ff3c20 !important;">
                             {{\App\CPU\Helpers::get_price_range($product) }}
+                            {{\App\CPU\Helpers::currency_converter($product->unit_price)}}
                         </span>
                         @if($product->discount > 0)
                             <strike style="color: #ff3c20 !important;">
